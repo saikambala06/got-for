ec52ca9913569ed20d82e964367d38cbe675268e
saikambala06 <saikambala777@gmail.com>
2026-07-04 00:58:33 +0530

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app = require('./api/index');
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`JobTrail server running at http://localhost:${PORT}`);
-});
