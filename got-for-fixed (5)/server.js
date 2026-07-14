1f5fe90c32683894d9fdd7422c5145139e1ebadd
saikambala06 <saikambala777@gmail.com>
2026-07-14 15:16:42 +0530

    Delete got-for-fixed (5) directory

--- got-for-fixed (5)/server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app = require('./api/index');
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`SKVK server running at http://localhost:${PORT}`);
-});
